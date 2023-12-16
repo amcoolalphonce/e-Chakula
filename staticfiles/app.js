804cb049f21dfdaa607d929337225d22f3556545
amcoolalphonce <82672024+amcoolalphonce@users.noreply.github.com>
2023-12-16 17:08:04 +0300

    Update app.js

---
+++ staticfiles/app.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', function () {
+        const menu = document.querySelector('#mobile-menu');
+        const menuLinks = document.querySelector('.nav-menu');
