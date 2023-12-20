fcbe81db649d0945b5d9543b7fb7834b6dfc7c7b
amcoolalphonce <82672024+amcoolalphonce@users.noreply.github.com>
2023-12-20 18:36:10 +0300

    Added the necessary models to the admin page

--- food/admin.py
+++ food/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
-from .models import Pizza
+from .models import Pizza, Beverages, Dishes, Special
 
 class PizzaAdmin(admin.ModelAdmin):
         list_display = ('name', 'priceL', 'priceM')
 
-
 admin.site.register(Pizza, PizzaAdmin)
+
+class BeveragesAdmin(admin.ModelAdmin):
+        list_display = ('name', 'price')
+
+admin.site.register(Beverages, BeveragesAdmin)
+
+class DishesAdmin(admin.ModelAdmin):
+        list_display = ('name', 'price')
+
+admin.site.register(Dishes, DishesAdmin)
+
+class SpecialAdmin(admin.ModelAdmin):
+        list_display = ('name', 'price')
+
+admin.site.register(Special, SpecialAdmin)
+
+
