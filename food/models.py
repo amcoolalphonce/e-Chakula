70d79eb43178ad5fbc2264cd02452070b39d5882
amcoolalphonce <82672024+amcoolalphonce@users.noreply.github.com>
2023-12-20 18:29:21 +0300

    dishes models

--- food/models.py
+++ food/models.py
@@ -10,5 +10,10 @@ class Pizza(models.Model):# should begin with a capital yawa
 class Beverages(models.Model):# should begin with a capital yawa
         name = models.CharField(max_length = 120)
         price = models.DecimalField(max_digits = 4, decimal_places =2)
-        pImage = models.URLField()
-        
+        bImage = models.URLField()
+
+class Dishes(models.Model):
+        name = models.CharField(max_length = 120)
+        price= models.DecimalField(max_digits = 4, decimal_places =2)
+        dImage = models.URLField()
+
