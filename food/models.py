3dab86345e2bb04533c920ad86df15b85260eee0
amcoolalphonce <82672024+amcoolalphonce@users.noreply.github.com>
2023-12-20 17:52:41 +0300

    Update models.py

--- food/models.py
+++ food/models.py
@@ -4,4 +4,4 @@ class pizza(models.Model):
         name = models.CharField(max_length = 120)
         priceM = models.DecimalField(max_digits = 4, decimal_places =2)
         priceL = models.DecimalField(max_digits = 4, decimal_places = 2)
-
+        pImage = models.URLField()
