b9364ad1ac492f00e502b8ed90d14e22d5a91a52
amcoolalphonce <82672024+amcoolalphonce@users.noreply.github.com>
2023-12-20 18:23:17 +0300

    Update admin.py

--- food/admin.py
+++ food/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Pizza
 
-admin.site.register(Pizza)
+class PizzaAdmin(admin.ModelAdmin):
+        list_display = ('name', 'priceL', 'priceM')
+
+
+admin.site.register(Pizza, PizzaAdmin)
