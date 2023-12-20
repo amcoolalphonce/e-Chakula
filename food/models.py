ec9ccab20dcfbbc0d7763d4ddd2731c94b0ccc57
amcoolalphonce <82672024+amcoolalphonce@users.noreply.github.com>
2023-12-20 17:52:08 +0300

    prices of pizza

--- food/models.py
+++ food/models.py
@@ -1,4 +1,7 @@
 from django.db import models
 
-class pizza(model.Model):
+class pizza(models.Model):
         name = models.CharField(max_length = 120)
+        priceM = models.DecimalField(max_digits = 4, decimal_places =2)
+        priceL = models.DecimalField(max_digits = 4, decimal_places = 2)
+
